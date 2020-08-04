7c131a3b73ff271839c8ccd5333d657387f21abe
Shamali Patil <shamalirpatil@gmail.com>
2020-08-04 13:46:55 +0100

    -Create Model and arrange code in MVC pattern.

--- QuizApp-4-8/ViewController.swift
+++ QuizApp-4-8/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     
     var questionNumber = 0
     var score = 0
+    
+    var quizHnadler = QuizHandler()
 
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -19,21 +21,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     @IBOutlet weak var progressBar: UIProgressView!
     
-    
-    
-   let quiz =  [Question(q: "A slug's blood is green.", a: "True"),
-                    Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
-                    Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
-                    Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
-                    Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
-                    Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
-                    Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
-                    Question(q: "Google was originally called 'Backrub'.", a: "True"),
-                    Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
-                    Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
-                    Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
-                    Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")]
-               
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -44,7 +31,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
-        let userGotCorrect = checkAnswer(userAnswer: sender.currentTitle!)
+        let userGotCorrect = quizHnadler.checkAnswer(userAnswer: sender.currentTitle!)
         
         if userGotCorrect {
             sender.backgroundColor = UIColor.green
@@ -52,45 +39,16 @@ class ViewController: UIViewController {
             sender.backgroundColor = UIColor.red
         }
         
-        nextQuestion()
+        quizHnadler.nextQuestion()
         
         Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
     }
     
-    func getQuestionText() -> String {
-        return quiz[questionNumber].question
-    }
-    
-    func nextQuestion()  {
-        if questionNumber + 1 < quiz.count {
-            questionNumber += 1
-        } else {
-            questionNumber = 0
-            score = 0
-        }
-    }
-    
-    func checkAnswer(userAnswer: String) -> Bool {
-        if userAnswer == quiz[questionNumber].answer {
-            score += 1
-            return true
-        } else {
-            return false
-        }
-    }
-    
-    func setProgressBar() -> Float {
-        return Float(questionNumber) / Float(quiz.count)
-    }
-    
-    func getScore() -> Int {
-        return score
-    }
     
     @objc func updateUI() {
-        questionLabel.text = getQuestionText()
-        progressBar.progress = setProgressBar()
-        scoreLabel.text = "Score \(getScore())"
+        questionLabel.text = quizHnadler.getQuestionText()
+        progressBar.progress = quizHnadler.setProgressBar()
+        scoreLabel.text = "Score \(quizHnadler.getScore())"
         
         trueButton.backgroundColor = UIColor.clear
         falseButton.backgroundColor = UIColor.clear
