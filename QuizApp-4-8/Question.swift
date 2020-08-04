31ba8d7dd73736c7023c51bc25c625cc04f6c2e5
Shamali Patil <shamalirpatil@gmail.com>
2020-08-04 11:09:07 +0100

    -Modify AppDelegate and SceneDelegate.

---
+++ QuizApp-4-8/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  QuizApp-4-8
+//
+//  Created by Shamali Patil on 04/08/2020.
+//  Copyright © 2020 Shamali Patil. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    var question : String
+    var answer : String
+    
+    init(q:String,a:String) {
+        self.question = q
+        self.answer = a
+    }
+}
